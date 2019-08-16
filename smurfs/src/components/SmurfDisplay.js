70312d3b0258f047e4156576f134f516f68052a3
Ben <allenben@protonmail.com>
2019-08-16 11:43:01 -0500

    Added components - installed Formik/axios

--- smurfs/src/components/SmurfDisplay.js
+++ smurfs/src/components/SmurfDisplay.js
@@ -1,3 +1,15 @@
 import React, { useContext } from "react";
 import { SmurfContext } from "../contexts/SmurfContext";
 import axios from "axios";
+
+
+
+
+function SmurfDisplay() {
+    return (
+      <div>
+      </div>
+    )
+  }
+  
+  export default SmurfDisplay;
